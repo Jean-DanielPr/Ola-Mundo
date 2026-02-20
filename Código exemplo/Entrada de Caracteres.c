30ef9b0c48c169a1143fca461dd3821481ed18b4
Jean Daniel da Silva Santos <jeandaniel78b@gmail.com>
2026-02-20 16:47:13 -0300

    Agora um programa que também lê caracteres
    Dessa vez fiz um programa que lê caracteres
    Ele pergunta meu nome, idade e peso
    E depois me mostra essas informações na tela.

---
+++ Código exemplo/Entrada de Caracteres.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]){
+
+    char nome[50];
+    int idade;
+    float peso;
+//Para declarar uma variavel char eu preciso colocar entre [] o numero de espaços que o sistema vai reservar para ela,
+
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+    printf("Digite sua idade: ");
+    scanf("%d", & idade);
+    printf("Digite seu peso: ");
+    scanf("%f", &peso);
+ //O comando (%s) vai ler uma sequencia de caracteres e (%c) só lê 1 caractere isolado.
+ //O comando (%d) ou (%i) Vai ler números inteiros. 
+ //O comando (%f) vai ler números REAIS.  
+
+    printf("Seu nome e %s.\n", nome);
+    printf("Sua idade e %d.\n", idade);
+    printf("Seu peso e %.2fKg.\n", peso);
+//No (%.2f) o .2 indica quantas casas decimais o programa vai me mostrar após a vírgula,
+    return 0;
+}
